b2b964060b6dc031fd6e2579aafee33c1deec576
Kian <146420159+KDuckyy@users.noreply.github.com>
2024-11-25 16:25:53 +0000

    Refactor About page layout and improve Header component structure

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,32 +3,36 @@ import Header from "../components/header";
 import * as HeroIcons from "@heroicons/react/24/outline";
 
 export default function About() {
-  return (
-    <div className="relative h-screen">
-      <Header />
-      <div className="absolute h-3/5 w-screen -z-50 top-0 left-0">
-        <Image
-          src={"/edinburgh.jpg"}
-          alt="Edinburgh castle"
-          style={{ objectFit: "cover" }}
-          fill={true}
-        />
-        <div className="relative mt-[50dvh] h-1/3 bg-gradient-to-b from-transparent to-white w-screen" />
-      </div>
-      <div className="relative flex flex-row justify-between mt-[50dvh] p-8 px-24">
-        <div className="flex flex-col items-center gap-2">
-          <HeroIcons.CheckIcon className="size-9 border border-black rounded-full p-1 shadow-lg" />
-          <span className="text-2xl">Who We Are</span>
+    return (
+        <div className="relative h-screen">
+            <Header />
+
+            <div className="absolute top-0 left-0 w-full -z-10">
+                <div className="relative w-full h-[900px]">
+                    <Image
+                        src={"/edinburgh.jpg"}
+                        alt="Edinburgh castle"
+                        className="object-cover"
+                        fill={true}
+                    />
+                    <div className="absolute bottom-0 left-0 bg-gradient-to-b from-transparent to-white w-screen h-1/3" />
+                </div>
+            </div>
+
+            <div className="relative flex flex-row justify-between p-8 px-24 mt-[55dvh]">
+                <div className="flex flex-col items-center gap-2">
+                    <HeroIcons.QuestionMarkCircleIcon className="size-9 border border-black rounded-full p-1 shadow-lg" />
+                    <span className="text-2xl">Who We Are</span>
+                </div>
+                <div className="flex flex-col items-center gap-2">
+                    <HeroIcons.TrophyIcon className="size-9 border border-black rounded-full p-1 shadow-lg" />
+                    <span className="text-2xl">Our Goal</span>
+                </div>
+                <div className="flex flex-col items-center gap-2">
+                    <HeroIcons.UsersIcon className="size-9 border border-black rounded-full p-1 shadow-lg" />
+                    <span className="text-2xl">Meet The Team</span>
+                </div>
+            </div>
         </div>
-        <div className="flex flex-col items-center gap-2">
-          <HeroIcons.CursorArrowRaysIcon className="size-9 border border-black rounded-full p-1 shadow-lg" />
-          <span className="text-2xl">Our Goal</span>
-        </div>
-        <div className="flex flex-col items-center gap-2">
-          <HeroIcons.UserIcon className="size-9 border border-black rounded-full p-1 shadow-lg" />
-          <span className="text-2xl">Meet The Team</span>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -2,20 +2,26 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-export default function header() {
+export default function Header() {
+    const currentPath = usePathname();
 
-  const currentPath = usePathname();
-
-  return (
-    <div className={`flex flex-row justify-between items-center p-3 text-zinc-200 select-none bg-zinc-900 bg-opacity-10 font-funnelSans`}>
-      <Link href="/" className="text-xl">ScotsFarm</Link>
-      <nav className="flex flex-row items-center gap-8">
-        <Link href="/about" className={`${currentPath === '/about' && 'text-white'}`}>About us</Link>
-        <Link href="/find-us">Location</Link>
-        <Link href="/contact" >
-          Contact us
-        </Link>
-      </nav>
-    </div>
-  );
+    return (
+        <div
+            className={`flex flex-row justify-between items-center p-3 text-zinc-200 select-none bg-zinc-900 bg-opacity-10 font-funnelSans z-50`}
+        >
+            <Link href="/" className="text-xl">
+                ScotsFarm
+            </Link>
+            <nav className="flex flex-row items-center gap-8">
+                <Link
+                    href="/about"
+                    className={`${currentPath === "/about" && "text-white"}`}
+                >
+                    About us
+                </Link>
+                <Link href="/find-us">Location</Link>
+                <Link href="/contact">Contact us</Link>
+            </nav>
+        </div>
+    );
 }
